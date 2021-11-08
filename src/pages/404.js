e124b0b0d7dfdc8f68d537ccb901c3efc4ab9ef3
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-08 13:57:48 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Questa strada non porta a nulla... L&#39;assenza di informazioni è essa stessa informazione.</p>
   </Layout>
 )
 
