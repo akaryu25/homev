b8d7781b2d17b3a226ad4e45d3acd96dc951c64a
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-08 17:05:06 +0100

    Update page1.js

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -8,9 +8,11 @@ import Seo from "../components/seo"
 const page1 = () => (
   <Layout>
     <Seo title="Page1" />
-    <p>Per risolvere questo eningma &egrave; necessario trovare il momento esatto.</p>
-    <p>Il primo codice contiene tale informazione, una volta che ne sarai in possesso procedi con lo step successivo.</p>
-    
+        <p>Questo &egrave; l'ultimo test per valutare le tue COmpetenze. Credo in te, so che sar&agrave; difficile ma penso che sarai in Grado di risolverlo.&nbsp;La soluzioNe ti pOrter&agrave; nel Mio giardino, dovE troverai gli altri concorrenti che sono riusciti a terminare l'enigma.&nbsp;</p>
+    <p>Sai, VOrrei tanto poter invitare tutti, ma per poter benefi<span style="color: #99cc00;">C</span>iAre dei su<span style="color: #99cc00;">o</span>i frutti serve dimostrare di esserne all'aLtez<span style="color: #99cc00;">z</span>a. E' un luogo p<span style="color: #99cc00;">e</span>nsato per <span style="color: #99cc00;">r</span>acc<span style="color: #99cc00;">o</span>glIere persone che <span style="color: #99cc00;">con</span>dividono la mia <span style="color: #99cc00;">s</span>tessa ide<span style="color: #99cc00;">o</span>logia, che mi ha portato a creare la prima mo<span style="color: #99cc00;">n</span>eta peer to peer, prima <span style="color: #99cc00;">an</span>cora di Bi<span style="color: #99cc00;">t</span>co<span style="color: #99cc00;">i</span>n e tutte le altre criptovalUte. Ho fallito, mi hanNO fermato prima che potessi renderla pubblica, sono stato allontanato dalla mia famiglia e ora vivo fuori dalla societ&agrave;. Dovevo aspettarmelo, la gente non era ancora pronta in quel momento...</p>
+    <p>Il mio allievo, Satoshi, &egrave; riuscito a portare alla luce la mia opera. Ora quello che vorrei fare &egrave; creare una societ&agrave; parallela, superiore, che non segua le regole corrotte di quella attuale. Che sappia leggere il <em>semaforo. </em>Che misuri con la mano destra le ore e con la mano sinistra i minuti.&nbsp;</p>
+    <p>La domanda &egrave; quindi: tu ne farai parte?</p>
+    <p>&nbsp; </p>
     <StaticImage
       src="../images/tesseract-first.png"
       width={300}
