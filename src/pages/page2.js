c50e5d17f737affe40d2a7916cb1f419bd12d644
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-08 13:59:54 +0100

    Create page2.js

---
+++ src/pages/page2.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Page2 = () => (
+  <Layout>
+    <Seo title="Page2" />
+    
+    <p>Risolvi enigma.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/tesseract-first.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="The first piece of the puzzle"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <StaticImage
+      src="../images/tesseract-third.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="The second piece of the puzzle"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <StaticImage
+      src="../images/tesseract-final.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="The final piece of the puzzle"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </p>
+  </Layout>
+)
+
+export default IndexPage
