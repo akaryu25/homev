d902760726d4158f82e59fcafabf1231fa51249e
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-10 15:16:22 +0100

    Update page2.js

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -9,7 +9,7 @@ const Page2 = () => (
   <Layout>
     <Seo title="Page2" />
     <p>...lunga ancora sicuramente &egrave; percorrere da strada la Ma. altro'nell o modo un in, migliori reso ha ci successo &egrave; che quello Tutto. ancora e Ancora. maturazione la per fondamentali sono errori gli tutti che idea'dell sono Io? influenzata stata fosse non vita nostra la e successo fosse non quello se ora mondo il sarebbe come immagini Ti</p>
-    <p>.1 2 6 13 19 26 34 45 56 il importante solo &egrave forse Ma</p>
+    <p>.1 2 6 13 19 26 34 45 56 il importante solo &egrave; forse Ma</p>
     <StaticImage
       src="../images/tesseract-third.png"
       width={300}
@@ -27,7 +27,7 @@ const Page2 = () => (
       alt="The final piece of the puzzle"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>Questo è tutto il necessario per risolvere l'enigma. Se serve
+    <p>Questo &egrave; tutto il necessario per risolvere l'enigma. Se serve
       <Link to="/page1/"> torna al momento</Link> <br />
     </p>
   </Layout>
