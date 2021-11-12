a60bd83ac1a7e14763f4d4077181ce573c6cbbed
akaryu25 <32076433+akaryu25@users.noreply.github.com>
2021-11-12 02:01:32 +0100

    Update and rename page1.js to indizio.js

---
+++ src/pages/indizio.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const indizio = () => (
+  <Layout>
+    <Seo title="Indizio" />
+    
+    <StaticImage
+      src="../images/indizio2.jpeg"
+      width={1200}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="The first piece of the puzzle"
+      style={{ marginBottom: `1.45rem` }}
+    />
+   
+   
+  </Layout>
+)
+
+export default indizio
